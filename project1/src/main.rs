9062ac33ff8d5ce2103c03dc3c47da637273b6e6
Fabian Gruber <fabian.gruber5@gmail.com>
2023-11-15 10:54:50 +0100

    impl vlcocks and concurrent pairs, visualization cleanup

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -5,58 +5,16 @@ use std::collections::{BTreeMap, HashMap};
 use std::error::Error;
 use std::fs::read_to_string;
 use std::str::FromStr;
-use strum::{EnumIter, IntoEnumIterator};
 
 const HLINE_PAD_X: u32 = 30;
 const HLINE_PAD_Y: u32 = 100;
 const EVENT_PAD_X: u32 = 50;
 const FONT_SIZE: f32 = 20.0;
 
-#[derive(EnumIter, Debug, PartialEq)]
-enum EventType {
-    Init,
-    MakingProgress,
-    Computing,
-    Checkpoint,
-    Send,
-    Receive,
-    ProcessingResults,
-}
-
-impl EventType {
-    fn style(&self) -> ShapeStyle {
-        match self {
-            Self::Init => ShapeStyle::from(&BLACK).filled(),
-            Self::MakingProgress => ShapeStyle::from(&BLACK),
-            Self::Computing => ShapeStyle::from(&BLACK),
-            Self::Checkpoint => ShapeStyle::from(&GREEN).filled(),
-            Self::Send => ShapeStyle::from(&BLUE).filled(),
-            Self::Receive => ShapeStyle::from(&RED).filled(),
-            Self::ProcessingResults => ShapeStyle::from(&BLACK),
-        }
-    }
-}
-
-impl FromStr for EventType {
-    type Err = Box<dyn Error>;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "Init event" => Ok(EventType::Init),
-            "Making progress" => Ok(EventType::MakingProgress),
-            "Computing" => Ok(EventType::Computing),
-            "Checkpoint" => Ok(EventType::Checkpoint),
-            "Send event" => Ok(EventType::Send),
-            "Receive event" => Ok(EventType::Receive),
-            "Processing results" => Ok(EventType::ProcessingResults),
-            _ => Err(format!("invalid event type {s}").into()),
-        }
-    }
-}
-
 #[derive(Debug)]
 struct Event {
-    event_type: EventType,
+    title: String,
+    v: Vec<usize>,
     host: String,
     clock: HashMap<String, usize>,
 }
@@ -65,10 +23,11 @@ impl FromStr for Event {
     type Err = Box<dyn Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (event_type, host_clock) = s.split_once('\n').unwrap();
+        let (caption, host_clock) = s.split_once('\n').unwrap();
         let (host, clock) = host_clock.split_once(' ').unwrap();
         Ok(Event {
-            event_type: event_type.parse().unwrap(),
+            title: caption.to_owned(),
+            v: Vec::new(),
             host: host.to_owned(),
             clock: serde_json::from_str(clock).unwrap(),
         })
@@ -77,11 +36,18 @@ impl FromStr for Event {
 
 fn draw_event(
     root: &DrawingArea<BitMapBackend, Cartesian2d<RangedCoordu32, RangedCoordu32>>,
+    label: char,
     x: u32,
     y: u32,
     style: ShapeStyle,
 ) -> Result<(), Box<dyn Error>> {
-    let event = Circle::new((x, y), 3, style);
+    let event = EmptyElement::at((x, y))
+        + Circle::new((0, 0), 5, style)
+        + Text::new(
+            String::from(label),
+            (5, 5),
+            ("sans-serif", FONT_SIZE).into_font(),
+        );
     root.draw(&event)?;
     Ok(())
 }
@@ -93,35 +59,17 @@ fn draw_hline(
     w: u32,
     color: RGBColor,
 ) -> Result<(), Box<dyn Error>> {
-    let event = EmptyElement::at((HLINE_PAD_X, y))
+    let hline = EmptyElement::at((HLINE_PAD_X, y))
         + Rectangle::new(
-            [(0, 0), (w as i32 - 2 * HLINE_PAD_X as i32, 1)],
+            [(0, 0), (w as i32 - 2 * HLINE_PAD_X as i32, 2)],
             ShapeStyle::from(&color).filled(),
         )
         + Text::new(
             String::from(label),
-            (-20, 5),
+            (0, 5),
             ("sans-serif", FONT_SIZE).into_font(),
         );
-    root.draw(&event)?;
-    Ok(())
-}
-
-fn draw_legend_element(
-    root: &DrawingArea<BitMapBackend, Cartesian2d<RangedCoordu32, RangedCoordu32>>,
-    label: &str,
-    x: u32,
-    y: u32,
-    style: ShapeStyle,
-) -> Result<(), Box<dyn Error>> {
-    let event = EmptyElement::at((x, y))
-        + Circle::new((0, 0), 3, style)
-        + Text::new(
-            String::from(label),
-            (15, -7),
-            ("sans-serif", FONT_SIZE).into_font(),
-        );
-    root.draw(&event)?;
+    root.draw(&hline)?;
     Ok(())
 }
 
@@ -135,27 +83,7 @@ fn draw_conn(
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let matches = command!()
-        .arg(arg!(<LOG_FILE> "Path to log file with events"))
-        .get_matches();
-
-    let path = matches.get_one::<String>("LOG_FILE").unwrap();
-    let data = read_to_string(path)?;
-    let events: Vec<&str> = data.lines().collect();
-    let events: Vec<Event> = events
-        .chunks(2)
-        .map(|e| (e[0].to_owned() + "\n" + e[1]).parse())
-        .collect::<Result<Vec<Event>, _>>()?;
-
-    let mut hosts = BTreeMap::new();
-    events.iter().for_each(|e| match hosts.get_mut(&e.host) {
-        Some(num) => *num += 1,
-        None => {
-            hosts.insert(e.host.clone(), 1);
-        }
-    });
-
+fn visualize(events: &[Event], hosts: &BTreeMap<String, usize>) -> Result<(), Box<dyn Error>> {
     let w = *hosts.values().max().unwrap() as u32 * EVENT_PAD_X + 3 * EVENT_PAD_X;
     let h = hosts.len() as u32 * HLINE_PAD_Y + HLINE_PAD_Y;
 
@@ -175,13 +103,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         draw_hline(&root, host, y, w, BLACK)?;
     }
 
-    for event in events {
-        let host = event.host;
+    for event in events.iter() {
+        let host = event.host.clone();
         let x = *event.clock.get(&host).unwrap() as u32 * EVENT_PAD_X + EVENT_PAD_X;
         let y = *host_ys.get(&host).unwrap();
-        draw_event(&root, x, y, event.event_type.style())?;
+        let label = event.title.chars().next().unwrap();
+        draw_event(&root, label, x, y, ShapeStyle::from(&BLACK).filled())?;
 
-        if let EventType::Receive = event.event_type {
+        if event.title.eq("Receive event") {
             let sender: Vec<String> = event
                 .clock
                 .keys()
@@ -196,14 +125,113 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let legend_y = h - 50;
-    let mut legend_x = 50;
-    for et in EventType::iter() {
-        let name = format!("{:?}", et);
-        draw_legend_element(&root, &name, legend_x, legend_y, et.style())?;
-        legend_x += 50 + 10 * name.len() as u32;
+    root.present()?;
+
+    Ok(())
+}
+
+fn assign_vector_clocks(events: &mut Vec<Event>, hosts: Vec<String>) {
+    let host_idxs: HashMap<String, usize> = hosts
+        .iter()
+        .enumerate()
+        .map(|(i, host)| (host.clone(), i))
+        .collect();
+    let mut vclocks = HashMap::new();
+    hosts.iter().for_each(|h| {
+        vclocks.insert(h, vec![0usize; hosts.len()]);
+    });
+
+    // TODO fails if log is not in cronological order
+    let mut messages = HashMap::<(String, usize), Vec<usize>>::new();
+
+    for event in events {
+        let host = event.host.clone();
+        // own vclock += 1
+        vclocks.get_mut(&host).unwrap()[*host_idxs.get(&host).unwrap()] += 1;
+
+        if event.title.eq("Receive event") {
+            // get sender and sender_clock from recv event
+            let sender: Vec<String> = event
+                .clock
+                .keys()
+                .map(String::to_owned)
+                .filter(|h| !h.eq(&host))
+                .collect();
+            let sender = sender.last().unwrap().clone();
+            let send_clock = event.clock.get(&sender).unwrap();
+
+            // get sender vclock from messages
+            let vec = messages.get(&(sender.clone(), *send_clock)).unwrap();
+
+            // update all but own vclock with max(v_msg[j], v[j])
+            hosts.iter().for_each(|h| {
+                if !h.eq(&host) {
+                    let j = *host_idxs.get(h).unwrap();
+                    vclocks.get_mut(&host).unwrap()[j] = vclocks.get(&host).unwrap()[j].max(vec[j]);
+                }
+            });
+        } else if event.title.eq("Send event") {
+            // store sender vclock in messages
+            messages.insert(
+                (host.clone(), *event.clock.get(&host).unwrap()),
+                vclocks.get(&host).unwrap().clone(),
+            );
+        }
+
+        event.v = vclocks.get(&host).unwrap().clone();
+    }
+}
+
+fn count_concurrent_events(events: &[Event]) -> usize {
+    let mut count = 0;
+    for e1 in events {
+        for e2 in events {
+            if e1.v.iter().zip(e2.v.iter()).any(|(e1, e2)| e1 > e2)
+                & e2.v.iter().zip(e1.v.iter()).any(|(e2, e1)| e2 > e1)
+            {
+                assert_ne!(e1.host, e2.host);
+                count += 1;
+                // println!("{e1:?} ||| {e2:?}");
+            }
+        }
+    }
+    count
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = command!()
+        .arg(arg!(<LOG_FILE> "Path to log file with events"))
+        .get_matches();
+
+    let path = matches.get_one::<String>("LOG_FILE").unwrap();
+    let data = read_to_string(path)?;
+    let events: Vec<&str> = data.lines().collect();
+    let mut events: Vec<Event> = events
+        .chunks(2)
+        .map(|e| (e[0].to_owned() + "\n" + e[1]).parse())
+        .collect::<Result<Vec<Event>, _>>()?;
+
+    let mut hosts = BTreeMap::new();
+    events.iter().for_each(|e| match hosts.get_mut(&e.host) {
+        Some(num) => *num += 1,
+        None => {
+            hosts.insert(e.host.clone(), 1);
+        }
+    });
+
+    visualize(&events, &hosts)?;
+
+    let hosts = hosts.into_keys().collect();
+
+    assign_vector_clocks(&mut events, hosts);
+
+    let count = count_concurrent_events(&events);
+    println!("Number of concurrent event pairs: {count}\n");
+
+    println!("Events with vector clocks:");
+    for event in events {
+        println!("{event:?}");
     }
 
-    root.present()?;
     Ok(())
 }

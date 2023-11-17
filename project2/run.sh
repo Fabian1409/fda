8102222a49cf0772c711abc400d8eb45fec862b1
Fabian Gruber <fabian.gruber5@gmail.com>
2023-11-17 21:00:41 +0100

    added run.sh

---
+++ project2/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# build
+cargo build --release
+
+# spawn $1 nodes
+for (( i = 0; i < $1; i++ ))
+do
+  echo $i
+  ./target/release/project2 $1 $i &
+  pids[${i}]=$!
+done
+
+# wait for all nodes
+for pid in ${pids[*]}; do
+    wait $pid
+done
